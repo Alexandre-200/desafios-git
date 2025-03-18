c9bb1993b45685666bcbdd6343b1b33e7966b861
Carlos Alexandre <alexandre_200@outlook.com.br>
2025-03-17 23:33:53 -0300

    inicializando JS

---
+++ script.js
@@ -0,0 +1,7 @@
+//Parte 1
+const numero = document.querySelector(".numero-principal");
+const botaoMenos = document.querySelector(".botao.decremento");
+const botaoMais = document.querySelector(".botao.incremento");
+const botaoReset = document.querySelector(".botao.reset");
+
+
